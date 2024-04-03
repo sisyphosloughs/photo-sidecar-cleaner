bec941e5d81775ae1e685edf5f921d65e31e697d
Benjamin Bruecker <bb@benjaminbruecker.de>
2024-04-03 19:30:12 +0200

    Refined and allowed multiple file extensions

--- photo-sidecar-cleaner.sh
+++ photo-sidecar-cleaner.sh
@@ -3,14 +3,14 @@
 # Checks if the necessary parameters were passed
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <DirectoryPath> <FileExtension> [ExcludeDirectories]"
-    echo "Example: $0 /path/to/folder xmp temp,@eaDir"
+    echo "Example: $0 /path/to/folder xmp,dop temp,@eaDir"
     exit 1
 fi
 
 # Sets the passed parameters
 SEARCH_DIR=$1 # The path of the directory to be searched
-FILE_EXTENSION=$2 # The file extension to search for
-EXCLUDE_DIRS=${3:-""} # Optional: A list of directories to be excluded from the search
+FILE_EXTENSIONS=$2 # A list of file extensions to search for
+EXCLUDE_DIRS=$3 # Optional: A list of directories to be excluded from the search
 
 # Determines the number of CPU cores for parallel processing
 CORES=$(nproc)
@@ -18,12 +18,12 @@ CORES=$(nproc)
 # Defines a function to process each found entry
 process_entry() {
     local entry=$1 # The full path to the file
-    local base="${entry%.*}" # Removes the file extension from the path
+    local base="${entry%%.*}" # Removes the file extension from the path
     local dir="$(dirname "$entry")" # Determines the directory of the file
     local filename="$(basename "$base")" # Extracts the filename without extension
     local found_files=$(find "$dir" -maxdepth 1 -type f -name "$filename.*" ! -name "$(basename "$entry")")
-    # Searches for files in the same directory that have the same name but a different extension
-    
+
+    # Searches for files in the same directory that have the same name but a different extension    
     if [ -n "$found_files" ]; then
         # Adds the found files to the FOUND_PAIRS list, if present
         echo "$found_files" >> "$FOUND_PAIRS"
@@ -62,9 +62,25 @@ if [ ! -z "$EXCLUDE_DIRS" ]; then
     fi
 fi
 
+# Construct find command's include string for file extensions
+INCLUDE_STRING=""
+if [ ! -z "$FILE_EXTENSIONS" ]; then
+    IFS=',' read -ra ADDR <<< "$FILE_EXTENSIONS" # Splits the FILE_EXTENSIONS variable into an array
+    if [ ${#ADDR[@]} -gt 0 ]; then
+        # Constructs the exclusion string for find, starting with the first directory
+        INCLUDE_STRING="-iname '*.${ADDR[0]}'"
+        for i in "${ADDR[@]:1}"; do
+            # Adds additional directories to the exclusion
+            INCLUDE_STRING="$INCLUDE_STRING -o -iname '*.$i'"
+        done
+        # Encloses the exclusion string in parentheses and appends it for the find command
+        INCLUDE_STRING="-type f \( $INCLUDE_STRING \)"
+    fi
+fi
+
 # Searches for files excluding the specified directories
 echo "Searching for files..."
-eval find "$SEARCH_DIR" $EXCLUDE_STRING -type f -iname "*.$FILE_EXTENSION" -print0 | xargs -0 -P "$CORES" -I {} bash -c 'echo "\""{}"\"" >> '"$FILES_TOTAL"
+eval find "$SEARCH_DIR" $EXCLUDE_STRING $INCLUDE_STRING -print0 | xargs -0 -P "$CORES" -I {} bash -c 'echo "\""{}"\"" >> '"$FILES_TOTAL"
 
 # Checks if the output file exists and is not empty
 if [ -s "$FILES_TOTAL" ]; then
